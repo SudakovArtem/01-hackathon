6c6bb4f93f70063ff797547353d83d229dff147f
AstakhovV <astakhclever@gmail.com>
2021-08-20 23:45:57 +0500

    Add fireworks, background and shape modules

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import './styles.css'
 import {ContextMenu} from "@/menu";
 import {SoundModule} from "@/modules/sound.module";
+import {BackgroundModule} from "./modules/background.module";
+import {ShapeModule} from "./modules/shape.module";
+import {FireworksModule} from "./modules/fireworks.module";
 
 const menuModules = [
-  new SoundModule('sound', 'Случайный звук'),
+    new SoundModule('sound', 'Случайный звук'),
+    new BackgroundModule('background', 'Изменить фон'),
+    new ShapeModule('shape', 'Создать фигуру'),
+    new FireworksModule('fireworks',  'Вкл./Откл. феерверк')
+
 ]
 
 const menu = new ContextMenu('#menu')
--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,15 @@
 import {Module} from '../core/module'
+import {random} from "@/utils";
 
 export class BackgroundModule extends Module {
-
+    constructor(type, text) {
+        super(type, text);
+    }
+    trigger() {
+        this.changeColorBody()
+    }
+    changeColorBody () {
+        const body = document.querySelector('body')
+        body.style.background = `#${random(0, 999999)}`;
+    }
 }
