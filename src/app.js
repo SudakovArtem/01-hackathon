85993af4c4f25edf7bfaed81284d677e56ac7695
ArtemSudakov <tyomik1000@gmail.com>
2021-08-20 14:37:39 +0300

    добавляет логику работы меню

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
 import './styles.css'
+import {ContextMenu} from "@/menu";
+import {SoundModule} from "@/modules/sound.module";
+
+const menu = new ContextMenu('#menu')
+
+menu.add(new SoundModule('sound', 'Случайный звук'))
 
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,32 @@
 import {Menu} from './core/menu'
 
 export class ContextMenu extends Menu {
+  constructor(selector) {
+    super(selector);
+    this.modules = []
+  }
 
+  open() {
+    this.menu.classList.add('open')
+    this.isOpen = true
+    this.menu.style.top = event.clientY + 'px'
+    this.menu.style.left = event.clientX + 'px'
+  }
+
+  close() {
+    this.menu.classList.remove('open')
+    this.isOpen = false
+    this.menu.style.top = event.clientY + 'px'
+    this.menu.style.left = event.clientX + 'px'
+  }
+
+  add(module) {
+    this.modules.push(module)
+    const htmlElement = module.toHTML()
+    htmlElement.addEventListener('click', (event) => {
+      event.preventDefault()
+      module.trigger()
+    })
+    this.menu.append(htmlElement)
+  }
 }
