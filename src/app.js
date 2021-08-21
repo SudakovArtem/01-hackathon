7eac69362fe75bf779355548004abb88b7f01ff8
Nero28 <shckiy.s@yandex.ru>
2021-08-21 14:47:38 +0300

    add countingClicks and randomImage

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
-import './styles.css'
-import {ContextMenu} from "@/menu";
-import {SoundModule} from "@/modules/sound.module";
-import {BackgroundModule} from "./modules/background.module";
-import {ShapeModule} from "./modules/shape.module";
-import {FireworksModule} from "./modules/fireworks.module";
+import "./styles.css";
+import { ContextMenu } from "@/menu";
+import { SoundModule } from "@/modules/sound.module";
+import { BackgroundModule } from "./modules/background.module";
+import { ShapeModule } from "./modules/shape.module";
+import { FireworksModule } from "./modules/fireworks.module";
+import { CountingClicks } from "./modules/countingСlicks.module";
+import { RandomImage } from "./modules/randomImage.module";
 
 const menuModules = [
-    new SoundModule('sound', 'Случайный звук'),
-    new BackgroundModule('background', 'Изменить фон'),
-    new ShapeModule('shape', 'Создать фигуру'),
-    new FireworksModule('fireworks',  'Вкл./Откл. феерверк')
+  new SoundModule("sound", "Случайный звук"),
+  new BackgroundModule("background", "Изменить фон"),
+  new ShapeModule("shape", "Создать фигуру"),
+  new FireworksModule("fireworks", "Вкл./Откл. феерверк"),
+  new CountingClicks("clicks", "Подсчет кликов"),
+  new RandomImage("image", "Случайное изображение"),
+];
 
-]
+const menu = new ContextMenu("#menu");
 
-const menu = new ContextMenu('#menu')
-
-menuModules.forEach((module) => menu.add(module))
-
+menuModules.forEach((module) => menu.add(module));
